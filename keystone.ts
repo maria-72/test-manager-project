f37288388442887101151185ef68abcf1f848a3f
Maria <klonmaria72@gmail.com>
2021-12-31 16:23:22 +0300

    initial commit

---
+++ keystone.ts
@@ -0,0 +1,16 @@
+import { config } from '@keystone-6/core';
+import { lists } from './schema';
+import { insertSeedData } from './seed-data';
+
+export default config({
+  db: {
+    provider: 'sqlite',
+    url: process.env.DATABASE_URL || 'file:./test.db',
+    async onConnect(context) {
+      if (process.argv.includes('--seed-data')) {
+        await insertSeedData(context);
+      }
+    },
+  },
+  lists,
+});
